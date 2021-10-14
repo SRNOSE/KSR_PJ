e0f2d78fa36a70ae0b5d21ccc7119cedbcdefd1e
SRNOSE <90014573+SRNOSE@users.noreply.github.com>
2021-10-14 13:33:12 +0900

    ss

--- MagazineB/js/common.js
+++ MagazineB/js/common.js
@@ -6,20 +6,28 @@ $(function(){
     //1.변수할당
     let all = $("#all");
 
-    let clo = $("#clo");
+    let clo = $("#close");
 
     let smsub = $(".smsub");
 
-    //기능2-1: #all메뉴 누르면 #all메뉴 사라지고 #close보여주기
+    //기능2: #all메뉴 누르면 #all메뉴 사라지고 #close, .smsub right값 주기
   
-   $("#all").click(function(){
-    
+   all.click(function(){
+        all.hide(300),
+        clo.show(300)
+        smsub.css({
+            right:"0%"
+        });
     });
 
     //기능3 : #close클릭하면 #close, .smsub 없어지고 #all보이기
 
-    $("#close").click(function(){
-      
+    clo.click(function(){
+      clo.hide(300),
+      all.show(300),
+      smsub.css({
+        right:"-200%"
+      });
     });
 
 });///////////제이쿼리 로드구역/////////////
