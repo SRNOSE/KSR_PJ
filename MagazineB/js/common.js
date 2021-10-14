da57096372b0ffdc615de585f4bb12de08147134
SRNOSE <90014573+SRNOSE@users.noreply.github.com>
2021-10-14 18:08:30 +0900

    ss

--- MagazineB/js/common.js
+++ MagazineB/js/common.js
@@ -30,4 +30,24 @@ $(function(){
       });
     });
 
+      $('.visual').slick({
+               autoplay : true,
+
+                dots: true,
+
+                speed : 500 /* 이미지가 슬라이딩시 걸리는 시간 */,
+
+                infinite: true,
+
+                autoplaySpeed: 4000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */,
+
+                arrows: true,
+
+                slidesToShow: 1,
+
+                slidesToScroll: 1,
+
+                fade: false,
+            });
+
 });///////////제이쿼리 로드구역/////////////
